637433485fa6c08b202c9fb695c632f58b6626f8
korrnel <korrnel@gmail.com>
2023-12-26 17:16:57 +0100

    day 04 02

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -9,58 +9,68 @@ fun main(args: Array<String>) {
             "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n" +
             "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
 
-    Day04.Game_04_01(inputData1.split("\n"),false)
-    Day04.Game_04_02(inputData1.split("\n"),false) // BAD
+    println( Day04.Game_04_01(inputData1.split("\n"),false))
+    println( Day04.Game_04_02(inputData1.trim().split("\n"),false))
 
-    Day04.Game_04_01(inputData.split("\n"),false)
-    Day04.Game_04_02(inputData.split("\n"),false) // BAD
+    println( Day04.Game_04_01(inputData.split("\n"),false))
+    println( Day04.Game_04_02(inputData.trim().split("\n"),false))
 }
 
 class Day04 {
     companion object {
 
-    fun Game_04_02(inputLines: List<String>, debug: Boolean) {
+    fun Game_04_02(inputLines: List<String>, debug: Boolean) : Int {
         var sum = 0
         var cards = mutableListOf<Int>()
-// init
-        inputLines.forEachIndexed { row, it ->
-            cards.add(row, 1)
-        }
+        var extraCards = mutableListOf<Int>()
         // collect numbers
-        inputLines.forEachIndexed { row, it ->
+        inputLines.forEachIndexed { i, it ->
             var numbers = mutableListOf<String>()
             for (i in it.indexOf(':') + 2..it.indexOf('|') - 1 step 3) {
                 // println(" -"+ it.substring(i)+ "-")
                 // println(" -"+ it.substring(i,i + 2)+ "-")
                 numbers.add(it.substring(i, i + 2).trim())
+
             }
-            var found = 0
+            var found = -1
             for (i in it.indexOf('|') + 2..it.length - 1 step 3) {
                 // println(" -"+ it.substring(i)+ "-")
                 // println(" -"+ it.substring(i,i + 2)+ "-")
                 if (numbers.contains(it.substring(i, i + 2).trim())) found = found + 1
 
             }
-
-//        println(found+1)
-            //      sum = sum + 1
-            if (found > 0) {
-                for (j in (1..found)) {
-                    cards.set((row + j), cards.get(row + j) + 1 * cards.get(row))
-                }
-            }
-            println(cards.get(row))
-            sum = sum + cards.get(row)
-            //  println(sum)
+            // how many winning cards?
+            cards.add(i,found+1)
+            extraCards.add(1)
+            if (debug) println(sum)
 
         }
+        println(cards)
+        // how many  cards there is ?
+        cards.forEachIndexed { index,it ->
+            val addition = extraCards[index] // how many we shoould add
+            if (cards[index]>0){ // if it is a hit
+                for (j in index+1 until index + 1 + it) {
+                    extraCards.set(j, addition+extraCards[j]) // add it
 
-        println(sum)
+                }
+            }
+
+        }
+        // now summarize
+        return (extraCards.sum())
     }
+        private fun process(cards: List<Int>, start: Int, end: Int): Int {
+            var ans = end - start
+            for (i in start until end) {
+                val wins = cards[i]
+                ans += process(cards, i + 1, i + 1 + wins)
+            }
+            return ans
+        }
 
-    fun Game_04_01(inputLines: List<String>, debug: Boolean) {
+            fun Game_04_01(inputLines: List<String>, debug: Boolean): Int {
         var sum = 0
-
         // collect numbers
         inputLines.forEachIndexed { i, it ->
             var numbers = mutableListOf<String>()
@@ -78,10 +88,10 @@ class Day04 {
 
             }
             if (found > -1) sum = sum + Math.pow(2.0, found.toDouble()).toInt()
-            println(sum)
+            if (debug) println(sum)
 
         }
-        println(sum)
+        return (sum)
     }
 
 
---
+++ src/main/kotlin/Day4_helper.kt
@@ -0,0 +1,51 @@
+/*
+* from here https://github.com/Jadarma/advent-of-code-kotlin-solutions/blob/main/solutions/aockt/y2023/Y2023D04.kt
+* but it gives the same....
+* */
+data class ScratchCard(val id: Int, val haveNumbers: Set<Int>, val winNumbers: Set<Int>) {
+
+    val numberOfWins: Int by lazy {
+        haveNumbers.intersect(winNumbers).size
+    }
+
+    val score: Long
+        get() {
+            val n = numberOfWins
+            if (n == 0) return 0L
+            var res = 1L
+            repeat(n - 1) {
+                res *= 2
+            }
+            return res
+        }
+}
+
+fun main() {
+    var data = mutableListOf<ScratchCard>()
+    val data2 = Common.getData(4)
+    data2.trim().split("\n").forEach{ line ->
+        val line2 = line.replace("  "," ")
+        val (card, contents) = line2.split(":")
+        val (win, have) = contents.split("|")
+        val cardId = card.trim().split(" ").mapNotNull { it.toIntOrNull() }.single()
+        val haveNumbers = have.trim().split(" ").map { it.toInt() }.toSet()
+        val winningNumbers = win.trim().split(" ").map { it.toInt() }.toSet()
+        data.add(ScratchCard(cardId, haveNumbers, winningNumbers))
+    }
+
+    // Part 1
+    println(data.sumOf { it.score })
+
+    // Part 2
+    println(process(data, 0, data.size))
+}
+
+private fun process(cards: List<ScratchCard>, start: Int, end: Int): Int {
+    var ans = end - start
+    for (i in start until end) {
+        val card = cards[i]
+        val wins = card.numberOfWins
+        ans += process(cards, i + 1, i + 1 + wins)
+    }
+    return ans
+}
