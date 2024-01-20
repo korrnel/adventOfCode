ad90ac0b08de26ed64aba31c9a796dccc2cb60fc
korrnel <korrnel@gmail.com>
2024-01-20 15:37:16 +0100

    cookie

--- src/main/kotlin/Common.kt
+++ src/main/kotlin/Common.kt
@@ -11,7 +11,7 @@ class Common {
             val client = HttpClient.newBuilder().build()
             val request = HttpRequest.newBuilder()
                 .uri(URI.create("https://adventofcode.com/2023/day/"+ day.toString() + "/input"))
-                .header("Cookie","_ga=GA1.2.295804623.1701507354; _gid=GA1.2.131787216.1701507354; session=53616c7465645f5fda3577ccfc12109342e8ec2e219ce4ccdc420e56d526eb4f59b00e396a73b67de8c5c4686db7b47d1bd6343dc6c28fd6fd35bce3818065d4; _ga_MHSNPJKWC7=GS1.2.1701511847.2.0.1701511847.0.0.0")
+                .header("Cookie","_ga=GA1.2.295804623.1701507354; session=53616c7465645f5fdc32da916c518d3393edaaa0247aeb85199d6fecd1523812773242e88beea220dbb162009b81e0b6d048d1185a617d21a9e0c6efddc82fda; _gid=GA1.2.1613609882.1705752355; _gat=1; _ga_MHSNPJKWC7=GS1.2.1705761165.81.1.1705761172.0.0.0")
                 .build()
             val response = client.send(request, HttpResponse.BodyHandlers.ofString())
             return  response.body()
