59f2c6eee4ff386b4934343b058473df3655c4af
korrnel <korrnel@gmail.com>
2024-02-18 09:00:13 +0100

    refact day10 , but part 2 is failling

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -6,38 +6,38 @@ fun main(args: Array<String>) {
     val inputData1 = "0 3 6 9 12 15\n" +
             "1 3 6 10 15 21\n" +
             "10 13 16 21 30 45"
+  // find the next element to the series
+    Day09.Game_01(inputData1.trim().split("\n"),false)
+    Day09.Game_02(inputData1.trim().split("\n"),false)
 
-    Day09.Game_09_01(inputData1.trim().split("\n"),false)
-    Day09.Game_09_02(inputData1.trim().split("\n"),false)
-
-    Day09.Game_09_01(inputData.trim().split("\n"),false)
-    Day09.Game_09_02(inputData.trim().split("\n"),false)
+    // find the previous element to the series
+    Day09.Game_01(inputData.trim().split("\n"),false)
+    Day09.Game_02(inputData.trim().split("\n"),false)
 }
 
 class Day09 {
     companion object {
 
-        fun Game_09_02(inputLines: List<String>, debug: Boolean) {
+        fun Game_02(inputLines: List<String>, debug: Boolean) {
         var sum: Long = 0
-        inputLines.forEach({ it ->
+        inputLines.forEach { it ->
             val numbers = it.split(" ").map { it.toLong() }
-            for (i in 0..numbers.size - 1) {
-                if (debug) print(" " + (numbers.get(i)))
-
+            for (element in numbers) {
+                if (debug) print(" " + (element))
             }
             if (debug) println()
             val x = moveUp(numbers)
             if (debug) println(x)
-            sum = sum + numbers.first() + x * -1
-            println(sum.toString() + " - " + numbers.first() + " " + x.toString())
-        })
-        println(sum)
+            sum += numbers.first() + x * -1
+            if (debug) println(sum.toString() + " - " + numbers.first() + " " + x.toString())
+        }
+            println(sum)
     }
 
     fun moveUp(numbers: List<Long>): Long {
         var numbers2 = mutableListOf<Long>()
         var max: Long = 0
-        for (i in 1..numbers.size - 1) {
+        for (i in 1 until numbers.size) {
             numbers2.add(numbers.get(i) - numbers.get(i - 1))
             print(" " + (numbers.get(i) - numbers.get(i - 1)))
         }
@@ -49,9 +49,9 @@ class Day09 {
         return (numbers2.first() + max * -1)
     }
 
-    fun Game_09_01(inputLines: List<String>, debug: Boolean) {
+    fun Game_01(inputLines: List<String>, debug: Boolean) {
         var sum: Long = 0
-        inputLines.forEach({ it ->
+        inputLines.forEach { it ->
             val numbers = it.split(" ").map { it.toLong() }
             for (i in 0..numbers.size - 1) {
                 if (debug) print(" " + (numbers.get(i)))
@@ -60,12 +60,14 @@ class Day09 {
             if (debug) println()
             val x = moveDown(numbers)
             if (debug) println(x)
-            sum = sum + numbers.last() + x
+            sum += numbers.last() + x
             println(sum.toString() + " - " + numbers.last() + " " + x.toString())
-        })
+        }
         println(sum)
     }
 
+
+        // find the last row where the difference is the same in a recursive function
     fun moveDown(numbers: List<Long>): Long {
         var numbers2 = mutableListOf<Long>()
         var max: Long = 0
--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,39 +1,105 @@
 fun main(args: Array<String>) {
 
-    val inputData = Common.getData(10, args[0])
+    val inputData = Common.getData(10, if (args.size>0) args.get(0) else "dev")
 
 
-    var inputData1 =
-        "..F7.\n" +
-    ".FJ|.\n" +
+    var inputData1 = "..F7.\n" +
+            ".FJ|.\n" +
             "SJ.L7\n" +
             "|F--J\n" +
             "LJ..."
-   // Day10.Game_10_01(inputData1.split("\n"),false)
 
-    Day10.Game_10_01(inputData.split("\n"),false)
+    val inputData2 = "...........\n" +
+            ".S-------7.\n" +
+            ".|F-----7|.\n" +
+            ".||.....||.\n" +
+            ".||.....||.\n" +
+            ".|L-7.F-J|.\n" +
+            ".|..|.|..|.\n" +
+            ".L--J.L--J.\n" +
+            "..........."
+   val inputData3 = "..........\n" +
+           ".S------7.\n" +
+           ".|F----7|.\n" +
+           ".||....||.\n" +
+           ".||....||.\n" +
+           ".|L-7F-J|.\n" +
+           ".|..||..|.\n" +
+           ".L--JL--J.\n" +
+           ".........."
+    val inputData4= ".F----7F7F7F7F-7....\n" +
+            ".|F--7||||||||FJ....\n" +
+            ".||.FJ||||||||L7....\n" +
+            "FJL7L7LJLJ||LJ.L-7..\n" +
+            "L--J.L7...LJS7F-7L7.\n" +
+            "....F-J..F7FJ|L7L7L7\n" +
+            "....L7.F7||L7|.L7L7|\n" +
+            ".....|FJLJ|FJ|F7|.LJ\n" +
+            "....FJL-7.||.||||...\n" +
+            "....L---J.LJ.LJLJ..."
+    val inputData5="FF7FSF7F7F7F7F7F---7\n" +
+            "L|LJ||||||||||||F--J\n" +
+            "FL-7LJLJ||||||LJL-77\n" +
+            "F--JF--7||LJLJ7F7FJ-\n" +
+            "L---JF-JLJ.||-FJLJJ7\n" +
+            "|F|F-JF---7F7-L7L|7|\n" +
+            "|FFJF7L7F-JF7|JL---7\n" +
+            "7-L-JL7||F7|L7F-7F7|\n" +
+            "L.L7LFJ|||||FJL7||LJ\n" +
+            "L7JLJL-JLJLJL--JLJ.L"
+    // Day10.Game_10_01(inputData1.split("\n"),false)
+
+    println( Day10.Game_01(inputData.split("\n"),false))
+    println( Day10.Game_02(inputData2.split("\n"),false))
+    println( Day10.Game_02(inputData3.split("\n"),false))
+    println( Day10.Game_02(inputData4.split("\n"),false))
+    println( Day10.Game_02(inputData5.split("\n"),false))
+
 }
 
 class Day10 {
     companion object {
+    var visitedPositions = mutableListOf<Pair<Int,Int>>()
+        fun Game_02(inputLines: List<String>, debug: Boolean):Int {
+            // init if needed
+            //println(visitedPositions)
+            init(inputLines,debug)
+        // init
+            val map = MutableList(inputLines.size) { MutableList(inputLines[0].length) { '0' } }
+            for (y in 0 until inputLines.size) {
+                    for (x in 0 until inputLines[y].length) {
+                        if (!visitedPositions.contains(Pair(x,y))) {
+                            map[y][x] = if (isIn(inputLines,Pair(x,y))) { '1' } else { '0' }
+                        } else {
+                            // if it is a border than it is not counts
+                            map[y][x] = inputLines[y][x]
+                        }
+                    }
+            }
 
-    fun Game_10_01(inputLines: List<String>, debug: Boolean) {
-        var sum: Int = 0
-        var positionX: Int
-        var positionY: Int
+            println(map)
+            return 1
+        }
 
-        val pos = findS(inputLines)
-        positionY = pos[0]
-        positionX = pos[1]
-
-
-        var StartPositionX = positionX
-        var StartPositionY = positionY
+        // ray trace,
+    fun isIn(inputLines: List<String>,pos: Pair<Int,Int>) : Boolean{
+        if (visitedPositions.contains(pos)) return false
+            // we have to count the border crossings
+            var borders = 0
+        for (i in pos.first .. 0 ) {
+            if (inputLines[pos.second][i] in setOf<Char>('L','J','7','F','-') ) borders+=1
+        }
+        // if the border corossing is odd so we are int
+        return borders % 2 == 1
+    }
+    fun init(inputLines: List<String>, debug: Boolean){
+        visitedPositions = mutableListOf<Pair<Int,Int>>()
+        // find the starting poision
+        var pos = findS(inputLines)?:Pair(0,0)
+        visitedPositions.add(pos)
         println(pos)
-
         // first move
-        positionY = pos[0] - 1
-        positionX = pos[1]
+        pos = Pair(pos.first,pos.second+1) // this has to be a legit step
         var direction: Int = 1
         /*
          1 up
@@ -42,33 +108,34 @@ class Day10 {
          4 right
          */
 //    println(inputLines)
-        while (!(StartPositionX == positionX && StartPositionY == positionY) && sum < 50000000) {
-            sum = sum + 1
+        // moving around
+        while (visitedPositions.get(0)!=pos) {
+            //  sum = sum + 1
             //   println(inputLines[positionY][positionX])
-            when (inputLines[positionY][positionX]) {
+            when (inputLines[pos.second][pos.first]) {
                 '|' -> {
                     when (direction) {
-                        1 -> positionY = positionY - 1
-                        2 -> positionY = positionY + 1
+                        1 -> pos = Pair(pos.first,pos.second - 1)
+                        2 -> pos = Pair(pos.first,pos.second + 1)
                     }
                 }
 
                 '-' -> {
                     when (direction) {
-                        3 -> positionX = positionX - 1
-                        4 -> positionX = positionX + 1
+                        3 -> pos = Pair(pos.first-1,pos.second)
+                        4 -> pos = Pair(pos.first+1,pos.second)
                     }
                 }
 
                 'L' -> {
                     when (direction) {
                         2 -> {
-                            positionX = positionX + 1
+                            pos = Pair(pos.first+1,pos.second)
                             direction = 4
                         }
 
                         3 -> {
-                            positionY = positionY - 1
+                            pos = Pair(pos.first,pos.second - 1)
                             direction = 1
                         }
                     }
@@ -77,12 +144,12 @@ class Day10 {
                 'J' -> {
                     when (direction) {
                         2 -> {
-                            positionX = positionX - 1
+                            pos = Pair(pos.first-1,pos.second)
                             direction = 3
                         }
 
                         4 -> {
-                            positionY = positionY - 1
+                            pos = Pair(pos.first,pos.second - 1)
                             direction = 1
                         }
                     }
@@ -91,12 +158,12 @@ class Day10 {
                 'F' -> {
                     when (direction) {
                         1 -> {
-                            positionX = positionX + 1
+                            pos = Pair(pos.first+1,pos.second)
                             direction = 4
                         }
 
                         3 -> {
-                            positionY = positionY + 1
+                            pos = Pair(pos.first,pos.second+1)
                             direction = 2
                         }
                     }
@@ -105,35 +172,38 @@ class Day10 {
                 '7' -> {
                     when (direction) {
                         1 -> {
-                            positionX = positionX - 1
+                            pos = Pair(pos.first-1,pos.second)
                             direction = 3
                         }
 
                         4 -> {
-                            positionY = positionY + 1
+                            pos = Pair(pos.first,pos.second + 1)
                             direction = 2
                         }
                     }
                 }
             }
-            println(sum)
+            visitedPositions.add(pos)
+            // this is the full circle length
+            if (debug) println(visitedPositions.size)
         }
 
-        println((sum + 1) / 2)
+    }
+    fun Game_01(inputLines: List<String>, debug: Boolean):Int {
+        // the furthest you can get, the half of the circle
+        init(inputLines,debug)
+        return ((visitedPositions.size + 1) / 2)
     }
 
-    fun findS(inputLines: List<String>): List<Int> {
-        var result = mutableListOf<Int>()
-        inputLines.forEachIndexed({ y, it ->
-            it.forEachIndexed({ x, it2 ->
+    fun findS(inputLines: List<String>): Pair<Int, Int>? {
+        inputLines.forEachIndexed { y, it ->
+            it.forEachIndexed { x, it2 ->
                 if (it2.equals('S')) {
-                    result.add(0, y)
-                    result.add(1, x)
-                    return result
+                    return Pair(x,y)
                 }
-            })
-        })
-        return result
+            }
+        }
+        return null
     }
 
 }
