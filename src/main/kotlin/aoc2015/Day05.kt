3f9f1e6d82dc60b6df3fd1547a63c5112b25d86b
Korrnel <korrnel@gmail.com>
2024-10-27 13:03:24 +0100

    2015 day 5 but why

--- src/main/kotlin/aoc2015/Day05.kt
+++ src/main/kotlin/aoc2015/Day05.kt
@@ -7,6 +7,43 @@ import java.security.MessageDigest
 
 /*
 
+--- Day 5: Doesn't He Have Intern-Elves For This? ---
+Santa needs help figuring out which strings in his text file are naughty or nice.
+
+A nice string is one with all of the following properties:
+
+It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+For example:
+
+ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
+aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
+jchzalrnumimnmhp is naughty because it has no double letter.
+haegwjzuvuyypxyu is naughty because it contains the string xy.
+dvszwmarrgswjxmb is naughty because it contains only one vowel.
+How many strings are nice?
+
+Your puzzle answer was 236.
+
+--- Part Two ---
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+For example:
+
+qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+How many strings are nice under these new rules?
+
+Your puzzle answer was 51.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
 
 * */
 fun main(args: Array<String>) {
@@ -18,9 +55,15 @@ fun main(args: Array<String>) {
                 "jchzalrnumimnmhp\n" + // naughty
                 "haegwjzuvuyypxyu\n" +// naughty
                 "dvszwmarrgswjxmb" // naughty
+        val inputData2 = "qjhvhtzxzqqjkmpb\n" + // nice
+                "xxyxx\n" + // nice
+                "uurcxstgmygtbstg\n" +// naughty
+                "ieodomkazucvgmuy" // naughty
 
     println(Day05.Game_01(inputData1.trim().split("\n"),false))
     println(Day05.Game_01(inputData.trim().split("\n"),false))
+    println(Day05.Game_02(inputData2.trim().split("\n"),true))
+    println(Day05.Game_02(inputData.trim().split("\n"),false))
 
     //  println(Day05.Game_02(inputData.trim().split("\n"),false))
 
@@ -55,9 +98,38 @@ class Day05 {
             }
 
             return nice
-    }
-        fun Game_02(inputLines: List<String>, debug : Boolean):Int {
-            return 0
         }
+        fun Game_02(inputLines: List<String>, debug : Boolean):Int {
+            var nice =0
+            inputLines.forEach {
+                if (debug) print(it+" ")
+                if (nice21(it) && nice22(it)) {
+                    nice++
+                    if (debug) println("nice")
+                }  else if (debug)  println("naughty")
+
+            }
+
+            return nice
+        }
+
+        fun nice21(input:String): Boolean {
+            // pairs
+            // It contains a pair of any two letters that appears at least twice in the string without overlapping,
+            // like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+            for (i in 1 until input.length-1) {
+                if (input.substring(i+1).contains(input.substring(i-1,i+1))) return true
+            }
+            return false
+        }
+        fun nice22(input:String): Boolean {
+            // repetion
+            // It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+            for (i in 0 until input.length-2) {
+                if (input[i].equals(input[i+2])) return true
+            }
+            return false
+        }
+
     }
 }
